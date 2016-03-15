c9edd93aa0507acd09b5d93a9bf12f9742227795
randalx2 <pmisthry@gmail.com>
2016-03-16 00:55:24 +0200

    Finalized code and demo'd polymorphism

--- Question4.cpp
+++ Question4.cpp
@@ -11,3 +11,93 @@
 // 4.2 From class "Animal", derive a concrete class "Bird" and "Dog".  Print appropriate message for
 //     each method.
 // 4.3 Write a driver program which creates an object of each type and demonstrates POLYMORPHISM. 
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+//Creating abtract class Animal with pure virtual functions
+
+class Animal{
+protected: string animalName, action;
+public: Animal(){
+			animalName = "no name";
+			action = "whatever";
+}
+		Animal(string iname, string iaction){
+			animalName = iname;
+			action = iaction;
+		}
+
+		~Animal(){
+
+		}
+	
+	    virtual void name() = 0;   
+		virtual void does() = 0;
+};
+
+class Bird : public Animal{
+public:
+	Bird(){
+		animalName = "Bird";
+		action = "Tweets and Flies";
+	}
+
+	Bird(string iname, string iaction){
+		animalName = iname;
+		action = iaction;
+	}
+
+	~Bird(){
+
+	}
+
+	void name(){
+		animalName = "Bird";
+	}
+
+	void does(){
+		action = "Tweets and Flies";
+	}
+};
+
+class Dog :public Animal{
+public:
+	Dog(){
+		animalName = "Dog";
+		action = "Barks and Woofs";
+	}
+
+	~Dog(){
+
+	}
+
+	void name(){
+		animalName = "Dog";
+	}
+
+	void does(){
+		action = "Barks and Woofs";
+	}
+	
+};
+
+int main(){
+	//Demonstrating polymorphism by creating pointers of the ABSTRACT CLASS Animal to dynamically create objects of the concrete
+	//sub classes Bird and Dog
+	//Cannot create objects of abstract classes but can create pointers of abstract classes
+
+	Animal *p1 = new Bird();
+	p1->name;
+	p1->does;
+
+	Animal *p2 = new Dog();
+	p2->name;
+	p2->does;
+
+	system("PAUSE");
+	return 0;
+}
